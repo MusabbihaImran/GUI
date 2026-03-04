421c05bcf024905c43b9fbba6c607f0c360bbd01
MusabbihaImran <imransabir783@gmail.com>
2026-03-04 09:35:53 +0500

    Initial Commit with files Activity1.py,Activity2.py, Activity3.py, Activity4.py, Activity5.py, Activity6.py, Task1.py, Task2.py, Task3.py, GUI.py, GUI2.py, Color.py, Loginpage.py, Lab4 Practice.py

--- Color.py
+++ Color.py
@@ -2,17 +2,16 @@ import tkinter as tk
 window=tk.Tk()
 window.geometry("600x500")
 window.title("Button")
-label=tk.Label(window,text="Initial Text", font=("Arial", 15, "bold"),bg= "purple", fg="pink", padx=10, pady=5)
-label.pack()
-button=tk.Button(window, text="Enter",font=("Arial", 12, "bold"),bg= "orange", fg="yellow", padx=10, pady=5, width=20, command=lambda:
-print("button pressed"), )
-button.pack()
-current_bg=button.cget("bg")
 def change():
+    current_bg = button.cget("bg")
     if current_bg=="orange":
         button.config(bg="yellow")
     else:
         button.config(bg="orange")
+label=tk.Label(window,text="Initial Text", font=("Arial", 15, "bold"),bg= "purple", fg="pink", padx=10, pady=5)
+label.pack()
+button=tk.Button(window, text="Enter",font=("Arial", 12, "bold"),bg= "orange", fg="black", padx=10, pady=5, width=20, command=change )
+button.pack()
 window.mainloop()
 
 
